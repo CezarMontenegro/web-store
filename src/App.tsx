c71dcc1cba24144a565afe0febfc16d05f88655c
Cezar <cezarguimaraes@gmail.com>
2024-01-22 13:05:10 -0300

    The request functions were set using axios in the api file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { getCategories, getProductByQuery, getProductByCategory, getProductByCategoryAndQuery } from './services/api'
+
 import './App.css'
 
 function App() {
-
   return (
     <div>
       
