c9407a6a125380d8410d67732212ac913b8255e3
Cezar <cezarguimaraes@gmail.com>
2024-02-06 12:10:50 -0300

    button was stylezed

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,12 +6,17 @@ function Header() {
   return (
     <Container>
       <img src="../../public/mercado-livre.svg" alt="logo" />
-      <input
-        type="text"
-        name="search-input"
-        placeholder="Buscar produtos, marcas e muito mais..." 
-        maxLength={120}
-      />
+      <div className='search-container'>
+        <input
+          type="text"
+          name="search-input"
+          placeholder="Buscar produtos, marcas e muito mais..." 
+          maxLength={120}
+        />
+        <button>
+        <i className="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </div>
       <div className='nav-header'>
           <Link to="shoppingCart"><i className="fa-solid fa-cart-shopping"></i></Link>
           <i className="fa-solid fa-circle-user"></i>
