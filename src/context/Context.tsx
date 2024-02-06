1e92ca48c98a411b05850919cc8848952fb04075
Cezar <cezarguimaraes@gmail.com>
2024-02-06 10:14:08 -0300

    creating context

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,16 +1,31 @@
-import { createContext, useState } from "react";
+import {createContext, useState, ReactNode, Dispatch, SetStateAction } from "react";
+import axios from 'axios';
+import { Category } from "../pages/home/Home";
 
-export const APIContext = createContext({
+//INTERFACES
+interface Props {
+  children: ReactNode;
+}
 
-});
+interface APIContextData {
+  productsByCategory: any[];
+  setProductsByCategory: Dispatch<SetStateAction<any[]>>
+  categories: Category[];
+  setCategories: Dispatch<SetStateAction<Category[]>>;
+  getCategories: () => Promise<void>;
+  getProductsByCategory: (CATEGORY_ID: string) => Promise<void>;
+}
 
-function APIContextProvider({ children }) {
-  const [productsByCategory, setProductsByCategory] = useState<any[]>([]);
-  const [categories, setCategories] = useState<Category[]>([]);
+//CRIAÇÃO DO CONTEXTO
+export const APIContext = createContext({} as APIContextData);
 
+//PROVIDER 
+function APIContextProvider({children}: Props) {
+  //ESTADOS
   const [productsByCategory, setProductsByCategory] = useState<any[]>([]);
   const [categories, setCategories] = useState<Category[]>([])
 
+  //FUNCOES
   const getCategories = async () => {
     try {
       const response = await axios.get('https://api.mercadolibre.com/sites/MLB/categories');
@@ -19,9 +34,6 @@ function APIContextProvider({ children }) {
       console.error(error)
     }
   }
-  useEffect(() => {
-    getCategories()
-  },[])
 
   const getProductsByCategory = async (CATEGORY_ID: string) => {
     try {
@@ -32,9 +44,18 @@ function APIContextProvider({ children }) {
     }
   }
 
+  const contextValue = {
+    productsByCategory,
+    setProductsByCategory,
+    categories,
+    setCategories,
+    getCategories,
+    getProductsByCategory,
+  }
+
   return (
     <APIContext.Provider value={contextValue}>
-      {children}
+      { children }
     </APIContext.Provider>
   );
 }
